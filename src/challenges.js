527940cea6cc41a8d85beb6ceb20c713f8fc29e2
acmarchiori <ac_marchiori@yahoo.com.br>
2022-10-27 20:14:35 -0300

    Desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -1,8 +1,8 @@
 // Desafio 1
 function compareTrue(girafa, elefante) {
   if (girafa && elefante === true) {
-  return true;
-}
+    return true;
+  }
   return false;
 }
 
@@ -10,11 +10,11 @@ function compareTrue(girafa, elefante) {
 const calcArea = (base, height) => (base * height) / 2;
 
 // Desafio 3
-const splitSentence = frase => frase.split(' ');
+const splitSentence = (frase) => frase.split(' ');
 
 // Desafio 4
 function concatName(frase) {
-  return (`${frase[frase.length-1]}, ${frase[0]}`);
+  return (`${frase[frase.length - 1]}, ${frase[0]}`);
 }
 
 // Desafio 5
@@ -29,15 +29,16 @@ function highestCount() {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  if ((mouse - cat1) < (mouse - cat2)){
-    return 'cat1';
+  let gatoPega = ' ';
+  const positionCat1 = Math.abs(mouse - cat1);
+  const positionCat2 = Math.abs(mouse - cat2);
+  (positionCat1 > positionCat2) ? gatoPega = 'cat2' : gatoPega = 'cat1';
+  if (positionCat1 === positionCat2) {
+    gatoPega = 'os gatos trombam e o rato foge';
+  }
+  return gatoPega;
 }
-else if ((mouse - cat1) > (mouse - cat2)) {
-    return 'cat2';
-}
-else if ((mouse - cat1) == (mouse - cat2)) {}
-    return 'os gatos trombam e o rato foge.'; 
-}
+console.log(catAndMouse(5, 3, 2));
 
 // Desafio 8
 function fizzBuzz() {
