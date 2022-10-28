312cf5f1bc3a71e778bdf9e3cbc17d75a1b745ff
acmarchiori <ac_marchiori@yahoo.com.br>
2022-10-27 21:30:42 -0300

    Desafio 10

--- src/challenges.js
+++ src/challenges.js
@@ -99,8 +99,20 @@ const decode = (frase) => {
 }
 
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(tecnologias, nome) {
+  let array = [];
+  if (tecnologias.length === 0) {
+    return 'Vazio!';
+  }
+  let tecnologia = tecnologias.sort();
+
+  for (let index = 0; index < tecnologias.length; index += 1) {
+    array.push({
+      tech: tecnologia[index],
+      name: nome,
+    });
+  }
+  return array;
 }
 
 module.exports = {
