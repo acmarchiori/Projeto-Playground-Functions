4c391aeee33901ef7c8fe99fee74b73663d7d25b
acmarchiori <ac_marchiori@yahoo.com.br>
2022-10-31 10:28:06 -0300

    Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,24 @@
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(telefone) {
+  if (telefone.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  } 
+  for (let i = 0; i < telefone.length; i += 1) {
+      let repetido = 0;
+      for (let index = 0; index < telefone.length; index += 1) {
+        if (telefone[i] == telefone[index]) {
+          repetido += 1;
+        }
+      }
+    
+      if (telefone[i] < 0 || telefone[i] > 9 || repetido >= 3) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+    }  
+    return `(${telefone[0]}${telefone[1]}) ${telefone[2]}${telefone[3]}${telefone[4]}${telefone[5]}${telefone[6]}-${telefone[7]}${telefone[8]}${telefone[9]}${telefone[10]}`;
+  }
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
-}
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
@@ -15,7 +32,6 @@ function triangleCheck(lineA, lineB, lineC) {
     return false;
   }
 }
-
 
 // Desafio 13
 function hydrate() {
