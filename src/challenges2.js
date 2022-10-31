0b06fa4607e50f986ad56d91b9c2786bb829059a
acmarchiori <ac_marchiori@yahoo.com.br>
2022-10-31 11:13:11 -0300

    Desafio 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -5,20 +5,17 @@ function generatePhoneNumber(telefone) {
   } 
   for (let i = 0; i < telefone.length; i += 1) {
       let repetido = 0;
-      for (let index = 0; index < telefone.length; index += 1) {
-        if (telefone[i] == telefone[index]) {
+      for (let i = 0; i < telefone.length; i += 1) {
+        if (telefone[i] == telefone[i]) {
           repetido += 1;
         }
       }
-    
       if (telefone[i] < 0 || telefone[i] > 9 || repetido >= 3) {
         return 'não é possível gerar um número de telefone com esses valores';
       }
     }  
     return `(${telefone[0]}${telefone[1]}) ${telefone[2]}${telefone[3]}${telefone[4]}${telefone[5]}${telefone[6]}-${telefone[7]}${telefone[8]}${telefone[9]}${telefone[10]}`;
   }
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
-
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
@@ -34,9 +31,19 @@ function triangleCheck(lineA, lineB, lineC) {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(bebidas) {
+  let numeros = /\d+/g;
+  let array = bebidas.match(numeros);
+  let soma = 0;
+  for(let i = 0; i < array.length; i += 1) {
+    soma = soma + parseInt(array[i]);
+  }
+  if(soma === 1) {
+    return `1 copo de água`
+  }
+  return `${soma} copos de água`;
 }
+
 
 module.exports = {
   generatePhoneNumber,
